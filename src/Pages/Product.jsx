9f4f0ab89ac0033db37d0f669b5b3840347977b4
liblissz <liblissz3@gmail.com>
2025-06-21 20:24:30 +0100

    Update Product.jsx

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,25 +1,29 @@
-
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext'; 
-import { useParams } from 'react-router-dom'
-import BreadCrums from '../Components/BreadCrums/BreadCrums'
+import { useParams } from 'react-router-dom';
+import BreadCrums from '../Components/BreadCrums/BreadCrums';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import Description from '../Components/DescriptionBox/Description';
 import RelatedProduct from '../Components/RelatedProducts/RelatedProduct';
 
 const Product = () => {
-  const {all_product} = useContext(ShopContext)
-  const {productId} = useParams()
-  const product = all_product.find((e)=> e.id === Number(productId))
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+
+  const product = all_product?.find((e) => e.id === Number(productId));
+
+  if (!product) {
+    return <div style={{ textAlign: 'center', padding: '50px' }}><h2>Loading product...</h2></div>;
+  }
+
   return (
     <div>
-      <BreadCrums product = {product}/>
-      <ProductDisplay product = {product} />
-      <Description/>
-      <RelatedProduct/>
+      <BreadCrums product={product} />
+      <ProductDisplay product={product} />
+      <Description />
+      <RelatedProduct />
     </div>
-  )
-}
+  );
+};
 
-export default Product
-
+export default Product;
